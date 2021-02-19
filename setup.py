a8df7cd44fbdb9a47baec26113385d4f6d6584f7
Piggey <49120531+Piggey@users.noreply.github.com>
2021-02-19 15:47:29 +0100

    ver 0.2

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ req = list(map(lambda v: v.rstrip('\n'), open("requirements.txt", 'r').readlines
 setup(
     name='osu_sr_calculator',
     author='Piggey',
-    version='0.1',
+    version='0.2',
     description="MrHelix's osu! star rating calculator rewritten for your Python needs",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/Piggey/osu-sr-calculator",
+    url="https://github.com/Piggey/osu_sr_calculator",
     install_reqs=req,
     keywords='osu!, osu, star rating, calculator, sr',
-    
-    packages=find_packages(include = ['osu_sr_calculator']),
+    packages=find_packages(include=['osu_sr_calculator']),
     include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
