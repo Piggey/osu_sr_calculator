c4e0b29b9d6647893c60a2633243599e27c03b92
Piggey <49120531+Piggey@users.noreply.github.com>
2021-02-22 22:40:39 +0100

    warnings and errors logs

--- osu_sr_calculator/Logger.py
+++ osu_sr_calculator/Logger.py
@@ -1,18 +1,24 @@
-class Logger(object):
+from warnings import warn
+
+class Logger:
     IsVerbose = False
-    def __init__(self, isVerbose):
+    File = None
+
+    def __init__(self, isVerbose, _file):
         self.IsVerbose = isVerbose
+        self.File = _file
 
     def log(self, message, log_type):
         """Available log_types:
         "msg", "warn", "error"
         """
-        if(self.IsVerbose):
-            if(log_type.lower() == 'msg'):
-                return f'[MSG] {message}'
-            elif(log_type.lower() == 'warn'):
-                return f'[WARN] {message}'
-            elif(log_type.lower() == 'error'):
-                return f'[ERROR!] {message}'
-        else:
-            return
+        # will log warnings and errors despite of verbosity
+        if(log_type.lower() == 'warn'):
+            warn(f'[WARN] [{self.File}] {message}', Warning)
+        
+        if(log_type.lower() == 'error'):
+            # dont really know if it should throw an exception but oh well
+            raise Exception(f'[ERROR] [{self.File}] {message}')
+
+        if(log_type.lower() == 'msg' and self.IsVerbose):
+            print(f'[MSG] [{self.File}] {message}')
