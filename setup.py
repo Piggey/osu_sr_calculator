7fd75f0ef37ae83b649d1751f3679428b9e5d1b5
Piggey <49120531+Piggey@users.noreply.github.com>
2021-06-02 14:59:15 +0200

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ packages = [
 setup(
     name='osu_sr_calculator',
     author='Piggey',
-    version='0.7.2',
+    version='0.7.3',
     description="MrHelix's osu! star rating calculator rewritten for your Python needs",
     long_description=readme,
     long_description_content_type="text/markdown",
